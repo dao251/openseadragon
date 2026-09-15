f270420b3619fc76c6b34c9c9de311a4aa12b416
Dmitrii Usevich <dima@epi.ru>
2026-09-15 10:37:47 +0100

    Demo: MVT Tile Source

---
+++ test/demo/pbf-wrapper.js
@@ -0,0 +1,4 @@
+import "https://unpkg.com/pbf/dist/pbf.js";
+// Re-export the classes from the global namespace
+export const PbfReader = window.Pbf.PbfReader;
+export const PbfWriter = window.Pbf.PbfWriter;
