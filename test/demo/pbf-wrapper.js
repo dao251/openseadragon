7e083428589692ea0640164cee2758505a2735e5
Dmitrii Usevich <dima@epi.ru>
2026-09-15 17:57:51 +0100

    Demo: MVT cleanup

--- test/demo/pbf-wrapper.js
+++
@@ -1,4 +0,0 @@
-import "https://unpkg.com/pbf/dist/pbf.js";
-// Re-export the classes from the global namespace
-export const PbfReader = window.Pbf.PbfReader;
-export const PbfWriter = window.Pbf.PbfWriter;
